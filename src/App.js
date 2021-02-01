058b3da8e5f1fcc05fe7f0de5fa0852196fe735b
Dushyant Pathak <dushyantpathak6789@gmail.com>
2021-02-02 01:19:51 +0530

    fix typo

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
   
 
   useEffect(() => {
-      if(speechState === speechState.Recording)
+      if(speechState === SpeechState.Recording)
           executeScroll()
   }, [])
   return (
