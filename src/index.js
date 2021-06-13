4b3fad1546dd8169d0a305b40d8d11f6716c9555
pkp2207 <58586336+pkp2207@users.noreply.github.com>
2021-06-13 11:12:45 +0530

    create const appId for easier rech

--- src/index.js
+++ src/index.js
@@ -7,16 +7,13 @@ import App from './App';
 import './assets/fonts/DejaVuSansMono.ttf';
 import { SpeechProvider } from '@speechly/react-client'
 
+const appId = "19de46da-14fc-412a-b1ae-1db9d21d8d99"
+
 ReactDOM.render(
-  <SpeechProvider appId="19de46da-14fc-412a-b1ae-1db9d21d8d99" language="en-US">
+  <SpeechProvider appId={appId} language="en-US">
       <Provider>
         <App />
       </Provider>
   </SpeechProvider>,
-  
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
